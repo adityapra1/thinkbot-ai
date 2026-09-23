3cc0fbddec6c9df4e2042c8d277daa1352be0f46
adityapra1 <adityaprakash.shibu@gmail.com>
2026-09-23 09:18:42 +0530

    Fixed API URL in utils.js safely

--- Frontend/src/utils/utils.js
+++ Frontend/src/utils/utils.js
@@ -1,5 +1,3 @@
-
-
-export const BACKEND_URL = import.meta.env.DEV 
-  ? "http://127.0.0.1:8000"       
-  : import.meta.env.VITE_BACKEND_URL;
+export const BACKEND_URL = import.meta.env.DEV
+  ? "http://127.0.0.1:8000"
+  : import.meta.env.VITE_API_URL;
